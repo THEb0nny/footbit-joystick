31598a63d4f3b44e07684468fd9e218bb6d1d97e
Dmitriy Antipov <aston_bonny@mail.ru>
2022-05-23 22:40:15 +0300

    Обновить pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -7,14 +7,16 @@ function Main () {
     radio.setGroup(252); // Групповой айди для общения джойстика и робота
     while (true) {
         basic.clearScreen(); // Очистить экран
-        let val_X = pins.analogReadPin(AnalogPin.P0); // Считать VRX
-        let val_Y = pins.analogReadPin(AnalogPin.P1); // Считать VRY
-        radio.sendValue("val_X", val_X); // Передать X
-        radio.sendValue("val_Y", val_Y);// Передать Y
-        let va_SW = pins.digitalReadPin(DigitalPin.P2); // Нажатие джойстика
+        let jValX = pins.analogReadPin(AnalogPin.P0); // Считать VRX
+        let jValY = pins.analogReadPin(AnalogPin.P1); // Считать VRY
+        let jBtnL = pins.digitalReadPin(DigitalPin.P2); // Нажатие джойстика
+        let jBtnR = pins.digitalReadPin(DigitalPin.P3); // Нажатие джойстика
+        radio.sendValue("jValX", jValX); // Передать X
+        radio.sendValue("jValY", jValY); // Передать Y
+        radio.sendValue("jBtnL", jBtnL); // Передать состоянии о нажатии левого джойстика
+        radio.sendValue("jBtnR", jBtnR); // Передать состоянии о нажатии левого джойстика
         // Вывод на экран направления
-        let col = val_X / segment_size;
-        let row = val_Y / segment_size;
+        let col = jValX / segment_size, row = jValY / segment_size;
         led.plot(col, row); // Вывод на матрицу направления
         basic.pause(10); // Задержка цикла
     }
